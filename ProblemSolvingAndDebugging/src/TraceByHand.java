d242572c150639fdb10eee27f1f90e96d8718b07
OrlandoFabian <orlando.marrero6@upr.edu>
2019-10-01 17:29:42 -0400

    Actually added TraceByHand

---
+++ ProblemSolvingAndDebugging/src/TraceByHand.java
@@ -0,0 +1,11 @@
+
+public class TraceByHand {
+	public static void main(String[] args) {
+		int f = 0, g = 1;
+		for(int i = 0; i <= 15; i++) {
+			System.out.println(f);
+			f = f + g;
+			g = f - g;
+		}
+	}
+}
